5683fe2f846f87065ae27606cf71fb22ee7e5614
Luis Cruz <ei06034@fe.up.pt>
2017-01-30 15:58:30 +0000

    changed project structure; fixed a few lint checks

---
+++ physalia/power_meters.py
@@ -0,0 +1,37 @@
+import abc
+import time
+
+
+class PowerMeter(object):
+    """Abstract class to implement interaction with a power monitor
+    """
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def start(self):
+        """Method to start measuring energy consumption
+        """
+        return
+
+    @abc.abstractmethod
+    def stop(self):
+        """Method to stop measuring energy consumption
+
+        Returns:
+            Energy consumption in Joules
+        """
+        return
+
+
+class EmulatedPowerMeter(PowerMeter):
+    """PowerMeter implementation to emulate a power monitor
+    """
+
+    def __init__(self):
+        self.start_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        return time.time() - self.start_time
