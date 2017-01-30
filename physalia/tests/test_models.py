1bd6abc47e080b0fb9a5ca726d1d74520e7b08d6
Luis Cruz <ei06034@fe.up.pt>
2017-01-30 14:13:39 +0000

    updataed database tests

--- physalia/tests/test_models.py
+++ physalia/tests/test_models.py
@@ -1,9 +1,14 @@
 import unittest
 from physalia.models import Measurement
+import os
 
 
 class TestMeasurement(unittest.TestCase):
+    TEST_CSV_STORAGE = "./test_db.csv"
+
     def test_persist(self):
+        Measurement.csv_storage = self.TEST_CSV_STORAGE
+        self.addCleanup(self.clearDatabase)
         measurement = Measurement(
             1485634263.096069,
             'login',
@@ -14,3 +19,10 @@ class TestMeasurement(unittest.TestCase):
             30
         )
         measurement.persist()
+        with open(self.TEST_CSV_STORAGE, 'r') as f:
+            content = f.read()
+        self.assertTrue(
+            """1485634263.096069,login,Nexus5X,1000,1000,2,30""" in content)
+
+    def clearDatabase(self):
+        os.remove(self.TEST_CSV_STORAGE)
