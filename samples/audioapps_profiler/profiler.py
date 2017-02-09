c9705ba66d96b58e0e4e7a3cf0cd76308b8fb0ad
Luis Cruz <ei06034@fe.up.pt>
2017-02-09 13:53:31 +0000

    sample to profile audio apps

---
+++ samples/audioapps_profiler/profiler.py
@@ -0,0 +1,154 @@
+"""Example of the usage of AndroidViewClientUseCase for
+with F-Droid.
+"""
+
+from time import sleep
+import subprocess
+from physalia.energy_profiler import AndroidViewClientUseCase
+
+# pylint: disable=invalid-name
+# this is just a script -- uppercase variables would look odd
+
+def get_running_pkg():
+    return subprocess.check_output(
+        "adb shell dumpsys window windows | grep mCurrentFocus | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev",
+        shell=True
+    ).strip()
+
+def run_open_app(use_case):
+    """Simply open app and wait 10 seconds."""
+    use_case.open_app()
+    sleep(10)
+
+def cleanup(use_case):
+    """Close the app and wait a second."""
+    use_case.kill_app()
+    sleep(1)
+
+
+youtube_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/YouTube Music_v1.62.7_apkpure.com.apk",
+    "com.google.android.apps.youtube.music",
+    "1.62.7",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+youtube_open_app_use_case.prepare_apk()
+youtube_open_app_use_case.run()
+youtube_open_app_use_case.profile_and_persist()
+
+spotify_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Spotify Music_v7.5.0.1076_apkpure.com.apk",
+    "com.spotify.music",
+    "7.5.0.1076",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+spotify_open_app_use_case.prepare_apk()
+spotify_open_app_use_case.run()
+spotify_open_app_use_case.profile_and_persist()
+
+google_play_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Google Play Music_v7.3.4313-1.M.3679657_apkpure.com.apk",
+    "com.google.android.music",
+    "7.3.4313.M.3679657",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+google_play_open_app_use_case.prepare_apk()
+google_play_open_app_use_case.run()
+google_play_open_app_use_case.profile_and_persist()
+
+
+soundcloud_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/SoundCloud Music Audio_v2017.01.24-release_apkpure.com.apk",
+    "com.soundcloud.android",
+    "2017.01.24-release",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+soundcloud_open_app_use_case.prepare_apk()
+soundcloud_open_app_use_case.run()
+soundcloud_open_app_use_case.profile_and_persist()
+
+mixerbox_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Free Music MP3 Player Download_v5.93_apkpure.com.apk",
+    "mbinc12.mb32b",
+    "5.93",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+mixerbox_open_app_use_case.prepare_apk()
+mixerbox_open_app_use_case.run()
+mixerbox_open_app_use_case.profile_and_persist()
+
+musichero_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Music Player Mp3 Player_v1.2.7_apkpure.com.apk",
+    "com.music.hero.music.player.mp3.free",
+    "1.2.7",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+musichero_open_app_use_case.prepare_apk()
+musichero_open_app_use_case.run()
+musichero_open_app_use_case.profile_and_persist()
+
+deezer_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Deezer Music Song Streaming_v5.4.0.88_apkpure.com.apk",
+    "deezer.android.app",
+    "5.4.0.88",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+deezer_open_app_use_case.prepare_apk()
+deezer_open_app_use_case.run()
+deezer_open_app_use_case.profile_and_persist()
+
+tubemp3_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Tube MP3 player music_v1.0.4_apkpure.com.apk",
+    "com.songsplayermp3.pt",
+    "1.0.4",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+tubemp3_open_app_use_case.prepare_apk()
+tubemp3_open_app_use_case.run()
+tubemp3_open_app_use_case.profile_and_persist()
+
+apple_music_open_app_use_case = AndroidViewClientUseCase(
+    "OpenApp",
+    "./apks/Apple Music_v1.2.0_apkpure.com.apk",
+    "com.apple.android.music",
+    "1.2.0",
+    run_open_app,
+    prepare=None,
+    cleanup=cleanup
+)
+
+apple_music_open_app_use_case.prepare_apk()
+apple_music_open_app_use_case.run()
+apple_music_open_app_use_case.profile_and_persist()
