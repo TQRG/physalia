855da7ce1fe2a2a5e7af37f2b17a620a88776149
Luis Cruz <ei06034@fe.up.pt>
2017-03-29 17:33:56 +0100

    added android utils

---
+++ physalia/utils/android.py
@@ -0,0 +1,21 @@
+"""Module with util functions to control Android devices."""
+
+import subprocess
+
+def set_charging_enabled(enabled, serialno=None):
+    """Enable or disable charging the device"""
+    if serialno:
+        command = (
+            "adb shell -s {serialno} dumpsys battery set ac {enabled};"
+            "adb shell -s {serialno} dumpsys battery set usb {enabled};"
+        ).format(serialno=serialno, enabled=int(enabled))
+    else:
+        command = (
+            "adb shell dumpsys battery set ac {enabled};"
+            "adb shell dumpsys battery set usb {enabled}"
+        ).format(enabled=int(enabled))
+
+    subprocess.check_output(
+        command,
+        shell=True
+    )
