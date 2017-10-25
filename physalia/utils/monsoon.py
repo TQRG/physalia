0b1696b097fc4f369318f4e1126b4e0577188ff7
Luis Cruz <ei06034@fe.up.pt>
2017-10-25 17:50:13 +0100

    is_monsoon_available for new firmware

--- physalia/utils/monsoon.py
+++ physalia/utils/monsoon.py
@@ -1,15 +1,16 @@
 """Module with util functions to use with Monsoon."""
 
-import os
-import sys
+import usb.core
+from  Monsoon.pmapi import USB_protocol
+
+def _device_matcher(d):
+    """Method copied from official Monsoon API"""
+    try:
+        return d.idVendor == 0x2AB9 and d.idProduct == 0x0001
+    except:
+        return False
 
 def is_monsoon_available():
     """Check wheter there is a Monsoon connnected."""
-    for dev in os.listdir("/dev"):
-        prefix = "ttyACM"
-        # Prefix is different on Mac OS X.
-        if sys.platform == "darwin":
-            prefix = "tty.usbmodem"
-        if dev.startswith(prefix):
-            return True
-    return False
+    device = usb.core.find(custom_match=_device_matcher)
+    return device is not None
